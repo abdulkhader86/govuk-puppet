540a7aef3467cd420b269e4ed1902fba1a4320d5
Sam Cook <sam.cook@digital.cabinet-office.gov.uk>
2017-04-11 16:54:35 +0100

    Upgrade version of bundler for ruby 1.9.3-p550 to 1.14.5
    This will enable us to use bundler's timeout_failover flag in
    conjunction with gemstash.

--- modules/govuk_rbenv/manifests/init.pp
+++ modules/govuk_rbenv/manifests/init.pp
@@ -8,7 +8,7 @@ class govuk_rbenv {
 
   unless $::lsbdistcodename == 'xenial' {
     rbenv::version { '1.9.3-p550':
-      bundler_version => '1.7.4',
+      bundler_version => '1.14.5',
     }
     rbenv::alias { '1.9.3':
       to_version => '1.9.3-p550',
